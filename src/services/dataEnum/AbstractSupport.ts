c66b25ff95594688e7b28068b85b275137dca2ca
Lanboo <549434510@qq.com>
2020-03-09 22:46:21 +0800

    UPDATE 下拉框枚举基类

--- src/services/dataEnum/AbstractSupport.ts
+++ src/services/dataEnum/AbstractSupport.ts
@@ -2,11 +2,15 @@ import { SelectDataEnum } from './SelectDataEnum';
 import { RecordData } from '../data';
 
 export abstract class AbstractSupport<T extends RecordData> {
-  protected selectDataEnum: SelectDataEnum<T> = {
+  selectDataEnum: SelectDataEnum<T> = {
     selectData: new Array<T>(),
     selectEnum: new Map(),
   };
 
-  //抽象方法 ，不包含具体实现，要求子类中必须实现此方法
-  abstract reload(): SelectDataEnum<T>;
+  reload(): SelectDataEnum<T> {
+    this.doReload();
+    return this.selectDataEnum;
+  }
+
+  protected abstract async doReload(): Promise<void>;
 }
