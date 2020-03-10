1a9d3a311f1105a81949326e67e993c8a049a379
Lanboo <549434510@qq.com>
2020-03-10 09:54:43 +0800

    UPDATE DicSupport增加按类型分组

--- src/pages/base/dic/DicSupport.tsx
+++ src/pages/base/dic/DicSupport.tsx
@@ -8,23 +8,40 @@ import { SelectDataEnum } from '@/services/dataEnum/SelectDataEnum';
 import { TableListItem } from './data';
 import { queryList } from './service';
 
+export interface DicSelectDataEnum extends SelectDataEnum<TableListItem> {
+  selectTypeData: Map<String, TableListItem[]>;
+}
+
 export class DicSupport extends AbstractSupport<TableListItem> {
   static type: String = 'dic';
   static support: DicSupport = new DicSupport();
-  static dataEnum: SelectDataEnum<TableListItem> = DicSupport.support.reload();
+  static dataEnum: DicSelectDataEnum = DicSupport.support.reload() as DicSelectDataEnum;
+
+  selectDataEnum: DicSelectDataEnum = {
+    selectData: new Array<TableListItem>(),
+    tableEnum: new Map(),
+    selectTypeData: new Map(),
+  };
 
   protected async doReload(): Promise<void> {
     queryList().then(data => {
       this.selectDataEnum.selectData = data;
 
       this.selectDataEnum.selectData.forEach(record => {
-        this.selectDataEnum.selectEnum[record.id] = (
+        this.selectDataEnum.tableEnum[record.id] = (
           <>
             <Text>{record.dicValue}</Text>
             <Text> </Text>
             <Text type="secondary">{record.dicDesc}</Text>
           </>
         );
+
+        let tempTypeList: TableListItem[] = this.selectDataEnum.selectTypeData[record.dicType];
+        if (!tempTypeList) {
+          tempTypeList = new Array<TableListItem>();
+          this.selectDataEnum.selectTypeData[record.dicType] = tempTypeList;
+        }
+        tempTypeList.push(record);
       });
     });
   }
